ca9f13560ed77f7f9de6c63b8d665ac6155bf6fa
ChienNguyenDac <chien.nguyen@enpii.com>
2021-09-28 06:09:32 +0700

    fix static file

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const app = express()
 // const messageIo = io.of("/")
 
 app.use(express.json())
-app.use(express.static(__dirname + 'public'))
+app.use(express.static('public'))
 app.use(cors())
 
 app.use('/api/auth', AuthRoute)
